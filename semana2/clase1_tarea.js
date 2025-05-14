0008b3d21b68f866df25912c1ef7370674ed1b50
dianarsit7 <diana.rsit99@gmail.com>
2025-05-13 21:20:34 -0600

    Hola

--- semana2/clase1_tarea.js
+++ semana2/clase1_tarea.js
@@ -3,7 +3,11 @@
 const { ask } = require('../helpers/input');
 
 async function main() {
-
+    for (let i = 0; i <=100; i++){
+        if (i % 2 === 0){
+        console.log (i)
+        }
+    }
 }
 
 main();
